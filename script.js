aaedbd9b55ef8daae09f3dbfe61b31a3ceb94bd8
victorandrespa <peralta.victor260@gmail.com>
2025-07-10 15:30:29 -0600

    cammbios

--- script.js
+++ script.js
@@ -101,3 +101,43 @@ contacts.push( {
 
 console.log(contacts[0].name +"/"+ contacts[0].phone +"/"+ contacts[0].email ) ;
 console.log(contacts[3].name +"/"+ contacts[3].phone +"/"+ contacts[3].email ) ;
+
+
+
+// Condicional execution
+
+let number = prompt ("Enter a random number");
+if (number > 90 && number < 110) {
+    alert("Bingo!");
+}else{
+    alert("Miss");
+}
+
+let number2 = prompt ("Enter a random number");
+let message = (number2 > 90 && number2 < 110) ? "Bingo" : "Miss";
+alert(message);
+
+
+let firstNumber = Number(prompt("Enter first number"));
+let secondNumber = Number(prompt("Enter second number"));
+let operand = prompt("Enter operand (+,-,* or /)");
+let result;
+
+if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
+    switch (operand) {
+        case "+": result = firstNumber + secondNumber;
+            break;
+        case "-": result = firstNumber - secondNumber;
+            break;
+        case "*": result = firstNumber * secondNumber;
+            break;
+        case "/": result = firstNumber / secondNumber;
+            break;
+    
+        default: result = "Error: unknown operand"
+    }
+} else {
+    result = "Error: at least one of the enterd values isnt a number"
+}
+alert(result);
+
